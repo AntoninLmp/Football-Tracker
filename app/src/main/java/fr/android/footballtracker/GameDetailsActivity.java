a963c4652efc500586ef171d830d0aadabf2563b
Kylian Sauvee <kylian@sauvee.com>
2023-04-06 17:10:34 +0200

    Add game details activity

---
+++ app/src/main/java/fr/android/footballtracker/GameDetailsActivity.java
@@ -0,0 +1,14 @@
+package fr.android.footballtracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_details);
+    }
+}
