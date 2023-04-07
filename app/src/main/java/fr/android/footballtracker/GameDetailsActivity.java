9a3e53e2cb3ff2469471de1d275503246b9d5803
Kylian Sauvee <kylian@sauvee.com>
2023-04-07 23:57:32 +0200

    Refactor activities code

--- app/src/main/java/fr/android/footballtracker/GameDetailsActivity.java
+++ app/src/main/java/fr/android/footballtracker/GameDetailsActivity.java
@@ -1,16 +1,13 @@
 package fr.android.footballtracker;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
 
 public class GameDetailsActivity extends AppCompatActivity {
 
@@ -24,23 +21,20 @@ public class GameDetailsActivity extends AppCompatActivity {
         bottomNavigationView = findViewById(R.id.bottomNavMenu);
         handler = new Handler();
 
-        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int id = item.getItemId();
-                if (id == R.id.menuCreate) {
-                    handler.post(()-> {
-                        Intent intent = new Intent(GameDetailsActivity.this, MatchCreation.class);
-                        startActivity(intent);
-                    });
-                } else if (id == R.id.menuHistory) {
-                    handler.post(()-> {
-                        Intent intent = new Intent(GameDetailsActivity.this, History.class);
-                        startActivity(intent);
-                    });
-                }
-                return true;
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.menuCreate) {
+                handler.post(()-> {
+                    Intent intent = new Intent(GameDetailsActivity.this, MatchCreation.class);
+                    startActivity(intent);
+                });
+            } else if (id == R.id.menuHistory) {
+                handler.post(()-> {
+                    Intent intent = new Intent(GameDetailsActivity.this, History.class);
+                    startActivity(intent);
+                });
             }
+            return true;
         });
     }
 }
--- app/src/main/java/fr/android/footballtracker/HomeActivity.java
+++ app/src/main/java/fr/android/footballtracker/HomeActivity.java
@@ -1,16 +1,11 @@
 package fr.android.footballtracker;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.MenuItem;
-import android.view.View;
 import android.widget.Button;
 
-import java.util.concurrent.atomic.AtomicReference;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -25,24 +20,14 @@ public class HomeActivity extends AppCompatActivity {
         newGameButton = findViewById(R.id.new_game_button);
         handler = new Handler();
 
-        historyButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                handler.post(() ->{
-                    Intent intent = new Intent(HomeActivity.this, History.class);
-                    startActivity(intent);
-                });
-            }
-        });
-        newGameButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                handler.post(() ->{
-                    Intent intent = new Intent(HomeActivity.this, MatchCreation.class);
-                    startActivity(intent);
-                });
-            }
-        });
+        historyButton.setOnClickListener(view -> handler.post(() ->{
+            Intent intent = new Intent(HomeActivity.this, History.class);
+            startActivity(intent);
+        }));
+        newGameButton.setOnClickListener(view -> handler.post(() ->{
+            Intent intent = new Intent(HomeActivity.this, MatchCreation.class);
+            startActivity(intent);
+        }));
     }
 
 
