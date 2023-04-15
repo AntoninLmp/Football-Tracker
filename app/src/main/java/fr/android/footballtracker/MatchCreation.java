f59ead46ebc429dc78b5b113067c0424e5f36131
Antonin Lampin <lampinantonin@gmail.com>
2023-04-15 23:45:29 +0200

    Save images in gallery

--- app/src/main/java/fr/android/footballtracker/MatchCreation.java
+++ app/src/main/java/fr/android/footballtracker/MatchCreation.java
@@ -1,23 +1,21 @@
 package fr.android.footballtracker;
 
 import android.Manifest;
-import android.content.ContentValues;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
 import android.icu.text.SimpleDateFormat;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 import android.provider.MediaStore;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -27,15 +25,17 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.util.Date;
 
 public class MatchCreation extends AppCompatActivity {
     private static final int PERMISSION_CAMERA_CODE = 100;
+    private static final int CAMERA_REQUEST_CODE = 1;
+
     BottomNavigationView bottomNavigationView;
     Handler handler;
     ImageButton buttonPhoto;
     Button buttonSave;
+    String currentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,30 +58,23 @@ public class MatchCreation extends AppCompatActivity {
             }
             return true;
         });
-        buttonPhoto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                askPermission();
-            }
-        });
-        buttonSave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MatchCreation.this, "Save", Toast.LENGTH_SHORT).show();
-            }
-        });
+        buttonPhoto.setOnClickListener(view -> handler.post(this::askPermission));
+        buttonSave.setOnClickListener(view -> handler.post(Toast.makeText(MatchCreation.this, "Save", Toast.LENGTH_SHORT)::show));
     }
 
     private void askPermission() {
         // Check if the permission has been already been given
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+        String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA};
+
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),permissions[0]) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this.getApplicationContext(), permissions[1]) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this.getApplicationContext(), permissions[2]) == PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PERMISSION_CAMERA_CODE);
         }else{
-            openCamera();
+            dispatchTakePictureIntent();
         }
-    }
-
-    private void openCamera() {
     }
 
     @Override
@@ -89,11 +82,52 @@ public class MatchCreation extends AppCompatActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == PERMISSION_CAMERA_CODE) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                openCamera();
+                dispatchTakePictureIntent();
             }else {
                 // It's means the user doesn't give the permission to use it
-                Toast.makeText(this, "Permission is required to use the camera", Toast.LENGTH_SHORT).show();
+                handler.post(Toast.makeText(this, "Permission is required to use the camera", Toast.LENGTH_SHORT)::show);
             }
         }
+    }
+
+    // This function is used to use the image to do something with
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PERMISSION_CAMERA_CODE && resultCode == Activity.RESULT_OK){
+            //Bitmap imageBitmap = (Bitmap) data.getExtras().get("data");
+            File newfile = new File(currentPhotoPath);
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an unique image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile("JPEG_" + timeStamp + "_", ".jpg", storageDir);
+        // Path for use
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        handler.post( () -> {
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            // Ensure that there's a camera activity to handle the intent
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                File photoFile = null; // Create the File where the photo should go
+                try {
+                    photoFile = createImageFile();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+                // Continue only if the File was successfully created
+                if (photoFile != null) {
+                    Uri photoURI = FileProvider.getUriForFile(this,"com.fr.android.footballtracker.android.fileprovider", photoFile);
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                    startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
+                }
+            }
+        });
     }
 }
